b42f963f8304566a5e7cddb1b6ad7ab23b699f9c
cvegao <constanza.vega@uv.es>
2023-11-09 14:30:30 +0100

    rand!, parseSMatrix, findall2

---
+++ src/MyUtils.jl
@@ -0,0 +1,41 @@
+module MyUtils
+
+using Distributions, StaticArrays
+
+export rand!, parseSMatrix, findall2
+
+function rand!(A::Array, distribution::Distribution)
+    for i in eachindex(A)
+        @inbounds A[i] = rand(distribution);
+    end
+    nothing
+end
+
+function rand!(A::Array, distribution::Array{T} where {T<:Distribution})
+    for i in eachindex(A)
+        @inbounds A[i] = rand(distribution[i]);
+    end
+    nothing
+end
+
+function parseSMatrix(A::Array)::SMatrix
+    return SMatrix{size(A)..., eltype(A)}(A)
+end
+
+"""
+Written by @jbrea [https://discourse.julialang.org/t/findall-slow/30247/5]
+"""
+function findall2(f, a::Array{T, N}) where {T, N}
+    j = 1;
+    b = Vector{Int}(undef, length(a));
+    @inbounds for i in eachindex(a)
+        @inbounds if f(a[i])
+            b[j] = i;
+            j += 1;
+        end
+    end
+    resize!(b, j-1);
+    sizehint!(b, length(b));
+    return b
+end
+end
